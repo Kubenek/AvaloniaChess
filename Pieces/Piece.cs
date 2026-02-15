6857e72bc4e2ba842c84976137a646f8ef888e76
Kubenek <Kubenku.of@gmail.com>
2026-02-15 11:27:41 +0100

    fix non-nullable Piece.cs warnings with constructor

--- Pieces/Piece.cs
+++ Pieces/Piece.cs
@@ -6,15 +6,21 @@ using Chess.Board;
 
 namespace Chess.Pieces
 {
-    public abstract class Piece(bool isWhite)
+    public abstract class Piece
     {
-        public bool IsWhite { get; set; } = isWhite;
+        public bool IsWhite { get; set; }
 
         public string Texture { get; protected set; }
-        public TextBlock pieceVisual {get; set; }
+        public TextBlock pieceVisual {get; set; } = new TextBlock();
 
         public int Row { get; set;}
         public int Column { get; set;}
+
+        public Piece(bool isWhite)
+        {
+            IsWhite = isWhite;
+            Texture = "";
+        }
 
         public abstract List<(int, int)> GetLegalMoves(ChessBoard board);
 
