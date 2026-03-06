8f431c1af24961fc56b18ba460c63e7a8fcf49de
Kubenek <Kubenku.of@gmail.com>
2026-03-06 11:10:58 +0100

    add pawn captures, minor fixes regarding pawn movement

--- Pieces/Pawn.cs
+++ Pieces/Pawn.cs
@@ -21,18 +21,30 @@ namespace Chess.Pieces
             int direction = IsWhite ? -1 : 1;
 
             var currentRow = Row + direction;
+            if(currentRow < 0 || currentRow >= 8) return moves;
+
             var currentCol = Column;
 
             var pieces = manager.pieces;
+
             var targetSquare = pieces[currentRow, currentCol];
+            if(targetSquare == null) {
 
-            if(targetSquare == null) moves.Add((currentRow, currentCol));
-            else return moves;
+                moves.Add((currentRow, currentCol));
 
-            if(doubleMove)
-            {
-                targetSquare = pieces[currentRow + direction, currentCol];
-                if(targetSquare == null) moves.Add((currentRow + direction, currentCol));
+                if(doubleMove)
+                {
+                    targetSquare = pieces[currentRow + direction, currentCol];
+                    if(targetSquare == null) moves.Add((currentRow + direction, currentCol));
+                }
+            }
+            if(currentCol-1 >= 0 && currentCol-1 < 8) {
+                var t1 = pieces[currentRow, currentCol-1];
+                if(t1 != null && t1.IsWhite != IsWhite) moves.Add((currentRow, currentCol-1));
+            }
+            if(currentCol+1 >= 0 && currentCol+1 < 8) {
+                var t2 = pieces[currentRow, currentCol+1];
+                if(t2 != null && t2.IsWhite != IsWhite) moves.Add((currentRow, currentCol+1));
             }
 
             return moves;
